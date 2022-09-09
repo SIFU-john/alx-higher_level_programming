973b8bc44ed671aa52f575a3fbfebd03fec591ef
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-09-09 18:58:52 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,13 +12,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	 if ( n > 0 ) {                  
-        printf ("is positive\n" ); 
+        printf ("is positive%d\n" ); 
     }
 	else if ( age < 0 ) {           
-        printf( "is negative\n" );       
+        printf( "is negative%d\n" );       
     }
     else {
-        printf( "is zero" );    
+        printf( "is zero%d\n" );    
     }
 	return (0);
 }
