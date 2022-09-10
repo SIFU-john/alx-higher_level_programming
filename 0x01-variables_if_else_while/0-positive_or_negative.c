a4f28a703e3a9bea3dc22d4b337926f12ad395c6
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-09-09 19:13:30 -0700

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,41 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-<<<<<<< HEAD
 #include <stdio.h>
-=======
->>>>>>> 3220e4884491ab78594aa18db91c9f32eccb82b5
-
 /**
-*main - Entry point
-*Return: Always
-/* betty style doc for function main goes there */
+* main - Entry point
+* Description: using main
+* Return: 0 Always
+*/
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-<<<<<<< HEAD
 if (n > 0)
 {
-    printf("%i is positive\n", n);
-} else if (n == 0)
+        printf("%i is positive\n", n);
+} 
+else if (n == 0)
 {
-    printf("%i is zero\n", n);
+        printf("%i is zero\n", n);
 }
- else if (n < 0)
+else if (n < 0)
 {
-    printf("%i is negative\n", n);
-=======
-if (n < 0)
-{
-printf("%d is negative\n", n);
-} else if (n == 0)
-{
-printf("%d is zero\n", n);
-} else
-{
-printf("%d is positive\n", n);
->>>>>>> 3220e4884491ab78594aa18db91c9f32eccb82b5
+        printf("%i is negative\n", n);
 }
 return (0);
 }
