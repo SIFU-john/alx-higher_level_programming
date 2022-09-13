b7a7661b2ab8ff7a511a62db3ab2a8a1018c0d6c
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-09-13 21:38:04 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
  * add - adds two nums
  *
- * @num1: first num
- * @num2: second num
+ * @num: initial num
+ * @numm: final num
  *
  * Return: sum of two nums
  */
 int add(int num, int numm)
-{
+{	
 	return (num + numm);
 }
