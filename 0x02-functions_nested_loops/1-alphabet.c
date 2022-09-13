f60e92f8cc3c72067eb0cfc224d5aa69122dca72
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-09-13 08:56:06 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ay = 'a';
+
+	while (ay <= 'z')
+	{
+		_putchar(ay);
+		ay++;
+	}
+
+	_putchar('\n');
+
+}
