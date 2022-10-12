49e7e6bd281667d649f0a7d6bf72f3cc1b8a914c
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-10-12 04:23:42 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
-#include "function_pointers.h"
-
+#include "function_pointers.h"i
 /**
  * main - check the code
+ * @name: name of the person
  * print_name - prints a name
  * @name: name to print
  * Return: Nothing.
