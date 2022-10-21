3a3d9a2cb2def988af3136a63d3c785d866509f5
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-10-20 22:01:59 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
  */
-size_t listint_len(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t num = 0;
+	size_t nodelen;
 
-	while (h)
+	nodelen = 0;
+	while (h != NULL)
 	{
-		num++;
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		nodelen++;
 	}
-
-	return (num);
+	return (nodelen);
 }
